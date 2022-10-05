3a0dbcdcdf707476e62b432e4e44d9252279b586
martin-ngigi <martinwainaina001@gmail.com>
2022-10-05 16:54:37 +0300

    Changed database from sqllite to postgresql

--- exepenseswebsite/exepenseswebsite/settings.py
+++ exepenseswebsite/exepenseswebsite/settings.py
@@ -75,8 +75,13 @@ WSGI_APPLICATION = 'exepenseswebsite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'incomeexpensesdb',
+        'USER': 'postgres',
+        'PASSWORD':'1234',
+        'HOST':'localhost'
     }
 }
 
