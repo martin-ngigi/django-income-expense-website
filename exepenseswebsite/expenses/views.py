fe20d8a022ce15d84f109a5192ed7bdba9caf173
martin-ngigi <martinwainaina001@gmail.com>
2022-10-06 10:17:24 +0300

    Render Expenses success and also login page

--- exepenseswebsite/expenses/views.py
+++ exepenseswebsite/expenses/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
+    #render template from templates/expenses/index.html
     return render(request, 'expenses/index.html')
 
 def add_expense(request):
