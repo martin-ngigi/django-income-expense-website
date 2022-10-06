60b53a1e28c17683fb6c5f496abef6fe33eb0c36
martin-ngigi <martinwainaina001@gmail.com>
2022-10-06 08:20:54 +0300

    Solved loading css bug

--- exepenseswebsite/exepenseswebsite/settings.py
+++ exepenseswebsite/exepenseswebsite/settings.py
@@ -124,8 +124,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+#NB SPELLING MISTAKE
+#exepenseswebsite\static
+#expenseswebsite/static
+
 STATIC_URL = 'static/'
-STATICFILES_DIRS =[os.path.join(BASE_DIR, 'expenseswebsite/static')]
+STATICFILES_DIRS =[os.path.join(BASE_DIR, 'exepenseswebsite\static')]
 STATIC_ROOT=os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
